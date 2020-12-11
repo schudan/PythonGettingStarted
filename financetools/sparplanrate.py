5348d49ff7e5a63ede7f1a1a959420f721475ee8
Schudan <schudan@gmail.com>
2020-12-11 20:59:11 +0100

    used  standard python code format conventions

--- financetools/sparplanrate.py
+++ financetools/sparplanrate.py
@@ -1,24 +1,23 @@
 def sparplan(stueckzahl, eurKurs, eurUsdKurs):
-   """Berechnet die Eingaben vom TradeRepublic-Ausdruck für die Eintragung 
-   in PortfolioPerformance """
+    """Berechnet die Eingaben vom TradeRepublic-Ausdruck für die Eintragung 
+    in PortfolioPerformance """
+    #ausgabe der Parameter
+    print("Berechne Sparplandaten für PortfolioPerformance")
+    print ("es wurden " + str(stueckzahl) + " Aktien zu " + str(eurKurs) + " gekauft.")
+    print ("dabei war der aktuel EUR/USD Umrechnungskurs " + str(eurUsdKurs))
+    #berechnungen
+    dollarKurs= eurKurs * eurUsdKurs
+    eurKosten = stueckzahl * eurKurs
+    #ausgabe der Ergebnisse
+    print("Kurs der Aktie in Dollar: " + str(dollarKurs))
+    print("Kosten in EUR: " + str(eurKosten))
 
-   print("Berechne Sparplandaten für PortfolioPerformance")
-   print ("es wurden "+ str(stueckzahl) + " Aktien zu " + str(eurKurs) + " gekauft.")
-   print ("dabei war der aktuel EUR/USD Umrechnungskurs " + str(eurUsdKurs))
-   
-   dollarKurs= eurKurs * eurUsdKurs
-   eurKosten = stueckzahl * eurKurs
-
-   print("Kurs der Aktie in Dollar: "+ str(dollarKurs))
-   print("Kosten in EUR: " + str(eurKosten))
-
+#programmstart
 #eingabe erfragen
-print("Stückzahl der Aktie")
+print("Stückzahl der Aktie") 
 stueckzahl = float(input())
-
-print ("EUR Kurs der Aktie")
+print ("EUR Kurs der Aktie") 
 eurKurs = float(input())
-
 print ("EUR/USD Kurs")
 eurUsdKurs = float(input())
 
